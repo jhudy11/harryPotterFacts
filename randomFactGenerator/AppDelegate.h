42a4b9d3edc1e0d5b9115cc23a8742acc0e48453
Joshua Hudson <jhudy11@hotmail.com>
2016-08-13 12:48:55 -0700

    Initial Commit

---
+++ randomFactGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  randomFactGenerator
+//
+//  Created by Joshua Hudson on 8/13/16.
+//  Copyright © 2016 Joshua Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ randomFactGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  randomFactGenerator
+//
+//  Created by Joshua Hudson on 8/13/16.
+//  Copyright © 2016 Joshua Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
